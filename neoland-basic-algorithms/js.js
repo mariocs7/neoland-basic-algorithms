ac474fe35cc3dc9461ddca72cf1a016112f68c79
Marius Cezar Stanca <mariuscezarstanca@MacBook-Air-de-Marius.local>
2024-01-15 21:22:37 +0100

    Ejercicio5

--- neoland-basic-algorithms/js.js
+++ neoland-basic-algorithms/js.js
@@ -35,3 +35,68 @@ const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
     
 
 //3.1
+console.log(10*5); 
+//3.2
+console.log(10/2); 
+//3.3
+console.log(15%9);
+//3.4
+let p = 10;
+let j = 5;
+let o = p + j;
+//3.5
+let c = 10
+let m = 5
+let i = c * m;
+
+//4.1
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengers[0]);
+//4.2
+avengers[0] = "IRON MAN";
+//4.3
+console.log(avengers.lenght)
+//4.4
+const richAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push("Morty", "Summer");
+console.log(richAndMortyCharacters[rickAndMortyCharacters.length - 1]);
+//4.5
+rickAndMortyCharacters.pop
+console.log(richAndMortyCharacters[0]);
+console.log(richAndMortyCharacters[richAndMortyCharacters.length -1]);
+//4.6
+rickAndMortyCharacters.splice(1, 1);
+console.log(rickAndMortyCharacters);
+
+//5.1
+const number1 = 10;
+const number2 = 20;
+const number3 = 2;
+
+if(number1 === 10){
+    console.log('number1 es estrictamente igual a 10')
+}
+
+if (number2/number1 == 2) {
+  console.log("number2 dividido entre number1 es igual a 2");
+}
+
+if (number1 /= number2) {
+  console.log("number1 es estrictamente distinto a number2");
+}
+
+if (number3 != number1) {
+  console.log("number3 es distinto number1");
+}
+
+if (number3*5 == number1) {
+  console.log("number3 por 5 es igual a number1");
+}
+
+if (number3*5 == number1 && number1*2 == number2) {
+  console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
+}
+
+if (number2/2 == number1 || number1/5 == number3) {
+  console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
+}
